9cfb0cd19f5ac123416e628605bdd4dad29d5d88
mikefront <mike_front@mail.ru>
2022-09-27 15:39:41 +0500

    Задание4 выполнил

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Задание3.1
+        // Задание 4
         int[] numbers = {1, 2, 3};
-        for (int i = numbers.length - 1; i >= 0; i--) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 1) {
+                numbers[i] ++;
+            }
             System.out.print(numbers[i]);
-            if (i != 0) {
-                System.out.print(",");
-            }
-        }
-        System.out.println();
-        // Задание3.2
-        double[] someNumbers = {1.57, 7.654, 9.986};
-        for (int j = someNumbers.length-1 ; j >= 0; j--) {
-            System.out.print(someNumbers[j]);
-            if (j != 0) {
-                System.out.print(",");
-            }
-        }
-        System.out.println();
-        // Задание3.3
-        int[] fruitBox = {4, 9, 15};
-        for (int k = fruitBox.length - 1; k >= 0; k--) {
-            System.out.print(fruitBox[k]);
-            if (k != 0) {
+            if (i != numbers.length -1) {
                 System.out.print(",");
             }
         }
