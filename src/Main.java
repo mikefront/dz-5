ec5282b734250f1f441fc34ca65f69f5ef685efa
mikefront <mike_front@mail.ru>
2022-09-27 12:30:57 +0500

    Задание3 выполнил

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Задание2.1
-        int[] numbers = new int[3];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        numbers[2] = 3;
-        for (int i = 0; i < numbers.length; i++) {
+        // Задание3.1
+        int[] numbers = {1, 2, 3};
+        for (int i = numbers.length - 1; i >= 0; i--) {
             System.out.print(numbers[i]);
-            if (i != numbers.length - 1) {
-                System.out.print(", ");
+            if (i != 0) {
+                System.out.print(",");
             }
         }
         System.out.println();
-        // Задание2.2
+        // Задание3.2
         double[] someNumbers = {1.57, 7.654, 9.986};
-        for (int j = 0; j < someNumbers.length; j++) {
+        for (int j = someNumbers.length-1 ; j >= 0; j--) {
             System.out.print(someNumbers[j]);
-            if (j != someNumbers.length - 1) {
-                System.out.print(", ");
+            if (j != 0) {
+                System.out.print(",");
             }
         }
         System.out.println();
-        // Задание2.3
-        int[] fruitBox = new int[15];
-        fruitBox[0] = 4;
-        fruitBox[1] = 9;
-        fruitBox[2] = 15;
-        for (int k = 0; k < fruitBox.length; k++) {
+        // Задание3.3
+        int[] fruitBox = {4, 9, 15};
+        for (int k = fruitBox.length - 1; k >= 0; k--) {
             System.out.print(fruitBox[k]);
-            if (k != fruitBox.length - 1) {
-                System.out.print(", ");
+            if (k != 0) {
+                System.out.print(",");
             }
         }
     }
